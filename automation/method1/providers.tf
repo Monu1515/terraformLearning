b60c308ba65defec96a6f57be30edb7d58775166
Monu1515 <91615213+Monu1515@users.noreply.github.com>
2022-04-10 21:04:40 +0530

    Update providers.tf

--- automation/method1/providers.tf
+++ automation/method1/providers.tf
@@ -9,8 +9,7 @@ terraform {
 
 
 provider "aws" {
-  access_key = "AKIARX4QDCSMDXXOLAPT"
-  secret_key = "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"
   region  = "us-east-1"
+  profile = "default"
 }
 
